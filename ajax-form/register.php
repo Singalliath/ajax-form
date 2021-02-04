50cf72d35404b2317e72fcf714577209e2820003
Singalliath Aldercroft <72815644+Singalliath@users.noreply.github.com>
2021-02-04 14:28:36 -0500

    Add files via upload
    link:https://www.youtube.com/watch?v=CaRZEdYRfaU 
    this is where I found that video that helped me make it.

---
+++ ajax-form/register.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Working Ajax</title>
+    <link rel="stylesheet" type="text/css" href="style.css" />
+</head>
+<body>
+<div class="workarea">
+
+<h1>The form has been submitted!</h1>
+<h2> Ajax is working correctly</h2>
+
+</div><!-- end of workarea -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<script type="text/javascript" language="javascript" src="script.js"></script>
+</body>
+</html>
